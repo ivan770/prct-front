49bc8c30074c0ba745fd6ca5e1a9f80a4df7c1e2
ivan770 <ivan@ivan770.me>
2022-05-11 16:03:45 +0100

    SPA init

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,14 @@
 <template>
-    <MainHeader />
-    <MainForm />
-    <MainFooter />
+    <RouterView />
 </template>
 
 <script>
-import MainHeader from './components/MainHeader.vue'
-import MainForm from './components/MainForm.vue'
-import MainFooter from './components/MainFooter.vue'
+import { RouterView } from "vue-router";
 
 export default {
-    name: 'App',
-    components: {
-        MainHeader,
-        MainForm,
-        MainFooter
-    }
-}
+  name: "App",
+  components: {
+    RouterView
+  },
+};
 </script>
-
-<style>
-#app {
-    font-family: "Roboto";
-    background-image: url("@/assets/Bg.svg");
-    background-size: cover;
-    min-width: 340px;
-    height: 100%;
-}
-
-.secondary-text {
-    color: var(--main-text-color);
-    font-size: 14px;
-}
-
-.flex-column {
-    display: flex;
-    flex-direction: column;
-}
-
-.space-between {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-}
-
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    position: relative;
-}
-
-:root {
-    --main-text-color: #566F96;
-    --button-dimmed: #AEDBCE;
-    --button-active: #30C598;
-}
-
-html,
-body {
-    height: 100%;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,34 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import VueApolloComponents from "@vue/apollo-components";
+import { createApolloProvider } from "@vue/apollo-option";
+import { createRouter, createWebHistory } from 'vue-router';
+import App from "./App.vue";
+import IndexView from './views/IndexView.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [{
+        path: "/",
+        name: "index",
+        component: IndexView,
+    }],
+});
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+    cache,
+    uri: "https://prct-api.elusive.space/graphql",
+});
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+});
+
+const app = createApp(App);
+
+app.use(router)
+    .use(apolloProvider)
+    .use(VueApolloComponents)
+    .mount("#app");
